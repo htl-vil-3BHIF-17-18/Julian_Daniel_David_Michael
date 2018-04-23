9697130c4fc499bf8d903492c86327a2ba8c27df
Michael <Michael@Michael-Pc>
2018-04-23 10:18:35 +0200

    CSVIO write read

--- src/dal/CSVIO.java
+++ src/dal/CSVIO.java
@@ -1,7 +1,9 @@
 package dal;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,13 +12,42 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import bll.Task;
+import bll.Task.FAECHER;
 
 public class CSVIO {
 	
 	public CSVIO() {}
 	
-	public void readTasks() {
+	public ArrayList<Task> readTasks() {
 		File file = chooseFile();
+		BufferedReader inputStream = null;
+		String l;
+		String[] splittedLine;
+		ArrayList<Task> newTasks = new ArrayList<Task>();
+		Task tempTask;
+		try {
+			inputStream = new BufferedReader(new FileReader(file.getAbsolutePath()));
+			while ((l = inputStream.readLine()) != null) {
+				splittedLine = l.split(";");
+				tempTask = new Task(FAECHER.valueOf(splittedLine[0]), splittedLine[1], splittedLine[2]);
+				newTasks.add(tempTask);
+			}
+		} catch (IOException e) {
+			System.err.println("Fehler beim Laden der Datei!");
+		} catch (NullPointerException e) {
+			System.err.println("Keine Datei wurde ausgewaehlt!");
+		} catch (NumberFormatException ex) {
+			System.err.println("Fehler beim Parsen der Katalognummer!");
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return newTasks;
 	}
 	
 	public void writeTasks(ArrayList<Task> tasks) {
