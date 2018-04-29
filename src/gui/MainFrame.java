793da8ea7a932c6c3fdc8eb07554a0a2de5661b2
schueler <schueler@ubuntu>
2018-04-29 15:55:27 -0700

    Mainfraim fertig gestellt

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -3,9 +3,11 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -18,6 +20,7 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
+import bll.Task;
 import dal.CSVIO;
 import dal.DatabaseIO;
 
@@ -36,21 +39,30 @@ public class MainFrame extends JFrame implements ActionListener {
 
 	// Panelright
 	private JPanel panelRight = null;
+	private JPanel panelList = null;
 	private JButton buttonHinzufuegen;
 	private JButton buttonErledigt;
+	private JButton buttonAndern;
 	private JButton buttonEntfernen;
 	private JTextField textfFach;
 	private JTextField textfAufgabe;
 	private JTextField textfDatum;
 
-	private JLabel platzhalter;
-	private JPanel nm;
-	private JButton notmaked;
-
 	// Radio Buttons unten
+	private JPanel pannelnotmaked;
 	private JRadioButton vergessen;
 	private JRadioButton nigesch;
+	
+	// Die zwei Listen
+	private TaskList liste;
+	private TaskList forgottenlist;
 
+	private JLabel platzhalter;
+	private JLabel platzhalter1;
+	private JLabel platzhalter2;
+	private JLabel platzhalter3;
+	private JLabel platzhalter4;
+	
 	public MainFrame() {
 		this.setTitle("Hausaufgabenplaner");
 		this.setSize(2000, 900);
@@ -68,54 +80,71 @@ public class MainFrame extends JFrame implements ActionListener {
 		BorderLayout grid = new BorderLayout();
 		this.setLayout(grid);
 
-		this.panelRight = new JPanel(new GridLayout(11, 1));
-		this.panelRight.setPreferredSize(new Dimension(450, 400));
-
+		menuBar = new JMenuBar();
+		this.menuBar.setPreferredSize(new Dimension(40, 60));
+		menuSave = new JMenu("Speicher");
+		menuItemDBSave = new JMenuItem("Datenbank");
+		menuItemCSVSave = new JMenuItem("CSV");
+		
+		this.panelRight = new JPanel(new GridLayout(12, 1));
+		this.panelRight.setPreferredSize(new Dimension(450, 40));
+		this.panelList = new JPanel(new GridLayout(11, 1));
+		this.panelList.setPreferredSize(new Dimension(40, 40));
+		this.pannelnotmaked = new JPanel(new GridLayout(4, 2));
+		this.pannelnotmaked.setPreferredSize(new Dimension(8, 75));
+	
 		this.buttonHinzufuegen = new JButton("Hinzuf�gen");
 		this.buttonErledigt = new JButton("Erledigt");
+		this.buttonAndern = new JButton("Aendern");
 		this.buttonEntfernen = new JButton("Entfernen");
 
 		this.textfFach = new JTextField("Fach:");
 		this.textfAufgabe = new JTextField("Aufgabe:");
 		this.textfDatum = new JTextField("Datum:");
 
-		this.platzhalter = new JLabel();
-		this.nm = new JPanel(new GridLayout(2, 1));
-		this.nm.setPreferredSize(new Dimension(100, 100));
-		this.nm.setBackground(Color.BLUE);
-
 		this.nigesch = new JRadioButton("nicht geschafft");
 		this.vergessen = new JRadioButton("vergessen");
-
 		ButtonGroup groupRadios = new ButtonGroup();
 		groupRadios.add(nigesch);
 		groupRadios.add(vergessen);
+		
+		this.liste = new TaskList(new ArrayList<Task>(), this);
+		this.liste.setPreferredSize(new Dimension(540, 500));
+		this.forgottenlist = new TaskList(new ArrayList<Task>(), this);
+		this.forgottenlist.setPreferredSize(new Dimension(50, 50));
 
-		menuBar = new JMenuBar();
-		this.menuBar.setPreferredSize(new Dimension(40, 60));
-		menuSave = new JMenu("Speicher");
-		menuItemDBSave = new JMenuItem("Datenbank");
-		menuItemCSVSave = new JMenuItem("CSV");
-
+		this.platzhalter = new JLabel();
+		this.platzhalter1 = new JLabel();
+		this.platzhalter2 = new JLabel();
+		this.platzhalter3 = new JLabel();
+		this.platzhalter4 = new JLabel();
+		
+		this.panelRight.add(platzhalter);
 		this.panelRight.add(textfFach);
 		this.panelRight.add(textfAufgabe);
 		this.panelRight.add(textfDatum);
-		this.panelRight.add(platzhalter);
+		this.panelRight.add(platzhalter1);
 		this.panelRight.add(buttonHinzufuegen);
 		this.panelRight.add(buttonErledigt);
+		this.panelRight.add(buttonAndern);
 		this.panelRight.add(buttonEntfernen);
-
+		this.panelRight.add(platzhalter2);
+		this.panelRight.add(platzhalter3);
+		this.panelRight.add(forgottenlist);
+	
 		this.menuSave.add(menuItemCSVSave);
 		this.menuSave.add(menuItemDBSave);
 		this.menuBar.add(menuSave);
 
-		this.nm.add(nigesch);
-		this.nm.add(vergessen);
-
+		this.pannelnotmaked.add(platzhalter4);
+		this.pannelnotmaked.add(nigesch);
+		this.pannelnotmaked.add(vergessen);
+		
 		this.add(this.panelRight, BorderLayout.LINE_END);
 		this.setJMenuBar(this.menuBar);
-		this.add(this.nm, BorderLayout.PAGE_END);
-
+		this.add(this.pannelnotmaked, BorderLayout.PAGE_END);
+		this.add(this.liste, BorderLayout.LINE_START);
+				
 		this.pack();
 		this.setSize(1000, 850);
 	}
