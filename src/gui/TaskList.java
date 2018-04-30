d03c0417370c571c379b521b3471182e8c19a0db
Michael <Michael@Michael-Pc>
2018-04-30 21:38:29 +0200

    Added TaskList

---
+++ src/gui/TaskList.java
@@ -0,0 +1,79 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+import bll.Task;
+
+public class TaskList extends JList<Task> implements MouseListener {
+
+	private static final long serialVersionUID = 1L;
+	private JScrollPane scrollPane;
+	private DefaultListModel<Task> model;
+	private JList<Task> taskList;
+	private MainFrame mainf;
+
+	public TaskList(ArrayList<Task> tasks, MainFrame mainf) {
+		this.setTasks(tasks);
+		this.mainf = mainf;
+		this.setPreferredSize(new Dimension(350, 500));
+		this.addMouseListener(this);
+		initializeControls();
+	}
+	
+	private void initializeControls() {
+		model = new DefaultListModel<Task>();
+		taskList = new JList<Task>();
+		taskList.setModel(model);
+		scrollPane = new JScrollPane(taskList);
+
+		taskList.setFont(this.getFont());
+		taskList.setBackground(Color.white);
+		taskList.addMouseListener(this);
+		taskList.setModel(model);
+		
+		this.add(scrollPane);
+	}
+
+	public void setTasks(List<Task> tasks) {
+		model.removeAllElements();
+		for (Task m : tasks) {
+			model.addElement(m);
+		}
+		this.setModel(model);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	public DefaultListModel<Task> getModel() {
+		return model;
+	}
+
+
+}
