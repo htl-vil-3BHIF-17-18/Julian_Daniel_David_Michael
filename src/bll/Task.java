d28f3fe5899a5a032a9b4e31468a84f53ae7219a
Michael <Michael@Michael-Pc>
2018-04-30 22:13:08 +0200

    Task status hinzugefuegt, offen erledigt vergessen nichtgeschaft

--- src/bll/Task.java
+++ src/bll/Task.java
@@ -8,14 +8,20 @@ public class Task {
 		AM, RK, POS, E, BWM_2, BWM_RW, DBI, BSPK, TINF, GGP_GEO, GGP_GPB, SYP, NW_CH
 	}
 
+	public enum STATUS {
+		OFFEN, ERLEDIGT, VERGESSEN, NICHTGESCHAFT
+	}
+
 	private FAECHER fach;
 	private String aufgabe;
 	private Date bisDatum;
+	private STATUS status;
 
 	public Task(FAECHER fach, String aufgabe, Date bisDatum) {
 		this.fach = fach;
 		this.aufgabe = aufgabe;
 		this.bisDatum = bisDatum;
+		this.status = STATUS.OFFEN;
 	}
 
 	public FAECHER getFach() {
@@ -40,6 +46,14 @@ public class Task {
 
 	public void setBisDatum(Date bisDatum) {
 		this.bisDatum = bisDatum;
+	}
+
+	public STATUS getStatus() {
+		return status;
+	}
+
+	public void setStatus(STATUS status) {
+		this.status = status;
 	}
 
 }
