ed893d04c8fdfd0903d26f48cb0fcd3c89b7ba9d
Michael <Michael@Michael-Pc>
2018-04-30 12:48:38 +0200

    Test

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,9 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
