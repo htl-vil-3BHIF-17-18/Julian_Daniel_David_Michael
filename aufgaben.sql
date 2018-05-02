38d314b2a9bd0262ba097618b76a8d9b7895a94c
Michael <Michael@Michael-Pc>
2018-05-02 16:27:01 +0200

    id

--- aufgaben.sql
+++ aufgaben.sql
@@ -1,8 +1,8 @@
-DROP TABLE tasks CASCADE CONSTRAINTS;
-
 CREATE TABLE tasks(
+  id INTEGER NOT NULL,
 	fach varchar(10) NOT NULL,
 	aufgabe varchar(50) NOT NULL,
-	datum date NOT NULL NOT NULL,
-	status VARCHAR(20)
+	datum date NOT NULL,
+	status VARCHAR(20),
+  CONSTRAINT pk PRIMARY KEY(id)
 );
